c792b5787307f0ecd4b23079e4da635aa64f32a7
Tom van meer <tom.vmeer@gmail.com>
2018-11-23 19:34:39 +0100

    Removed old data set demographics.csv. Added module timeIt for timing executing time of functions. Fixed error in showDatasetsPage.py to no longer look for text files, as we are switching to a database.

--- pages/scripts/preProcessing.py
+++ pages/scripts/preProcessing.py
@@ -4,4 +4,6 @@ import pandas as pd
 def read_data(encoded_data, filename, content_type) -> pd.DataFrame:
     if not encoded_data:
         raise(Exception("markoerror"))
+    print(encoded_data.decode())
+    print(filename, content_type)
     return pd.DataFrame({'test':[1,2,3,4,5,6]})  # TEST
