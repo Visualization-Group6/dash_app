6f4b4748b11e3190e6ab8fbb938afabb68b5102a
20175552 <markopetkovic1999@gmail.com>
2018-11-23 13:01:05 +0100

    marko test

--- pages/scripts/preProcessing.py
+++ pages/scripts/preProcessing.py
@@ -4,4 +4,4 @@ import pandas as pd
 def read_data(encoded_data, filename, content_type) -> pd.DataFrame:
     if not encoded_data:
         raise(Exception("markoerror"))
-    return pd.DataFrame({'test':[1,2,3,4,5,6]})
+    return pd.DataFrame({'test':[1,2,3,4,5,6]})  # TEST
