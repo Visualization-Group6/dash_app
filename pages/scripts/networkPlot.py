40909703202799207b206a834d2e9c8c0fb4a10f
Tom van Meer <tom.vmeer@gmail.com>
2018-11-25 19:19:35 +0100

    Test with network plots

--- pages/scripts/networkPlot.py
+++ pages/scripts/networkPlot.py
@@ -1,77 +1,83 @@
 import plotly.plotly as py
 import plotly.graph_objs as go
 from plotly import offline
-
+import numpy as np
 import networkx as nx
-
 #  https://plot.ly/~empet/14683/networks-with-plotly/#/
 #  https://plot.ly/python/network-graphs/
 
-G = nx.random_geometric_graph(200,0.125)
-nx.write_gml(G, 'testlist.txt')
-#G = nx.read_edgelist('testlist.txt')
-pos = nx.get_node_attributes(G,'pos')
+G = nx.read_edgelist("testgraph.txt")
 
-edge_trace = go.Scatter(
-    x=[],
-    y=[],
-    line=dict(width=0.5,color='#888'),
-    hoverinfo='none',
-    mode='lines')
+weights = []
+for edge in G.edges(data=True):
+    weights.append(edge[2]['weight']*0.6)
 
-for edge in G.edges():
-    x0, y0 = G.node[edge[0]]['pos']
-    x1, y1 = G.node[edge[1]]['pos']
-    edge_trace['x'] += tuple([x0, x1, None])
-    edge_trace['y'] += tuple([y0, y1, None])
+labels = []
+i = 0
+for node in G.nodes(data=True):
+    labels.append(node[0])
+    i += 1
 
-node_trace = go.Scatter(
-    x=[],
-    y=[],
-    text=[],
-    mode='markers',
-    hoverinfo='text',
-    marker=dict(
-        showscale=True,
-        # colorscale options
-        #'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
-        #'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
-        #'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
-        colorscale='YlGnBu',
-        reversescale=True,
-        color=[],
-        size=10,
-        colorbar=dict(
-            thickness=15,
-            title='Node Connections',
-            xanchor='left',
-            titleside='right'
-        ),
-        line=dict(width=2)))
+tree_pos = nx.fruchterman_reingold_layout(G, weight='weight')
+pos = {}
+for key in tree_pos:
+    pos[key] = list(tree_pos[key])
 
-for node in G.nodes():
-    x, y = G.node[node]['pos']
-    node_trace['x'] += tuple([x])
-    node_trace['y'] += tuple([y])
+nx.set_node_attributes(G,pos,name='pos')
+nx.write_gml(G,"testgraph_out.txt")
 
 
-for node, adjacencies in enumerate(G.adjacency()):
-    node_trace['marker']['color']+=tuple([len(adjacencies[1])])
-    node_info = '# of connections: '+str(len(adjacencies[1]))
-    node_trace['text']+=tuple([node_info])
 
-fig = go.Figure(data=[edge_trace, node_trace],
+
+
+edge_trace = [dict(type='scatter',
+             x=[pos[e[0]][0], pos[e[1]][0]],
+             y=[pos[e[0]][1], pos[e[1]][1]],
+              mode='lines',
+              line=dict(width=weights[k], color='black', ))  for k, e in enumerate(G.edges())]
+
+#edge_trace = go.Scatter(
+#    x=[],
+#    y=[],
+#    line=dict(width=0.5,color='#888'),
+#    hoverinfo='none',
+#    mode='lines')
+
+#for edge in G.edges():
+#    x0, y0 = G.node[edge[0]]['pos']
+#    x1, y1 = G.node[edge[1]]['pos']
+#    edge_trace['x'] += tuple([x0, x1, None])
+#    edge_trace['y'] += tuple([y0, y1, None])
+
+xs, ys = [], []
+for key in pos:
+    xs.append(pos[key][0])
+    ys.append(pos[key][1])
+
+nodes=dict(type='scatter',
+           x=xs,
+           y=ys,
+           mode='markers',
+           hoverinfo='text',
+           marker=dict(size=20, color='red'),
+           text=labels)
+
+#for node in G.nodes():
+#    x, y = G.node[node]['pos']
+#    node_trace['x'] += tuple([x])
+#    node_trace['y'] += tuple([y])
+
+#for node, adjacencies in enumerate(G.adjacency()):
+#    node_info = str(labels[node])
+#    node_trace['text']+=tuple([node_info])
+
+fig = go.Figure(data=edge_trace+[nodes],
              layout=go.Layout(
-                title='<br>Network graph made with Python',
+                title='<br>Holliday destinations',
                 titlefont=dict(size=16),
                 showlegend=False,
                 hovermode='closest',
                 margin=dict(b=20,l=5,r=5,t=40),
-                annotations=[ dict(
-                    text="Python code: <a href='https://plot.ly/ipython-notebooks/network-graphs/'> https://plot.ly/ipython-notebooks/network-graphs/</a>",
-                    showarrow=False,
-                    xref="paper", yref="paper",
-                    x=0.005, y=-0.002 ) ],
                 xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
                 yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)))
 
