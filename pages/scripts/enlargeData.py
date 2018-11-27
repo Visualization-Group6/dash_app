5ed6c7baee790c57ceccbc7fc08b59999fd0d71d
Tom van Meer <tom.vmeer@gmail.com>
2018-11-27 11:18:49 +0100

    Added preProcessing update by marko

---
+++ pages/scripts/enlargeData.py
@@ -0,0 +1,26 @@
+from pages.scripts import preProcessing
+
+working_dir = preProcessing.get_working_dir()
+
+with open(working_dir + "profile_semantic_trafo_final.txt", "r") as f:
+    data = f.read()
+
+for i in range(10):
+    print(i)
+    dat = data.split("\n")[3:]
+    newdata = []
+    for i in range(1, len(dat), 2):
+        newtime = int(dat[i].split(" ")[0]) + 1
+        newline = str(newtime) + " " + " ".join(dat[i].split(" ")[1:])
+        newdata.append("")
+        newdata.append(newline)
+
+
+    old = data.split("\n")
+    for i in newdata:
+        old.append(i)
+
+    data = "\n".join(old)
+
+with open(working_dir + "large.txt", "w") as f:
+    f.write(data)
--- pages/scripts/preProcessing.py
+++ pages/scripts/preProcessing.py
@@ -1,9 +1,50 @@
 import pandas as pd
+import numpy as np
+import dash_html_components as html
+import time
+import pickle
+import os
 
 
-def read_data(encoded_data, filename, content_type) -> pd.DataFrame:
-    if not encoded_data:
-        raise(Exception("markoerror"))
-    print(encoded_data.decode())
-    print(filename, content_type)
-    return pd.DataFrame({'test':[1,2,3,4,5,6]})  # TEST
+# Petitie om dit te renamen naar enlargePenis.py
+
+def get_working_dir():
+    wdr = os.getcwd()
+    dirs = wdr.split("\\")
+    working_dir = "/".join(dirs[:1+dirs.index("dash_app")])
+    working_dir += "/datasets/"
+    return working_dir
+
+
+def save_data(encoded_data, filename, content_type):
+    working_dir = get_working_dir()
+    with open(working_dir + filename, 'w') as f:
+        f.write(encoded_data.decode())
+
+
+def open_dataset(filename):
+    working_dir = get_working_dir()
+    with open(working_dir + filename, 'r') as f:
+        encoded_data = f.read()
+    print("read data")
+    # process data in usable format
+    new_data = [i.strip().split(" ") for i in encoded_data.split('\n') if i != ""]
+    print("data formatted")
+    # we use new_data[1:] because we want to skip the line with the title
+    # check time length
+    time_length = max([int(i[0]) for i in new_data[1:] if len(i) == 4])
+    print("got times")
+    # maybe slightly change what's below
+    nodes = max([max([int(i[1]) for i in new_data[1:] if len(i) == 4]),
+                 max([int(i[2]) for i in new_data[1:] if len(i) == 4])])  # finds the highest node number
+    print("got nodes")
+    network_data = np.empty((nodes + 1, nodes + 1, time_length + 1))  # add 1 since arrays start with 0
+
+    for i in new_data[1:]:
+        if len(i) == 4:
+            network_data[int(i[1]), int(i[2]), int(i[0])] = int(i[3])
+
+    print(network_data.shape)
+    return pd.DataFrame({'test': [network_data[498, 486, 1]]})  # TEST
+    # return pd.DataFrame({ 'test': [1,2,3]})
+
