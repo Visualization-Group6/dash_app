17c94c4ba3ab8a2ffee5413e9acaef486e979346
Tom van Meer <tom.vmeer@gmail.com>
2018-11-29 19:05:31 +0100

    Minor changes to rensPage Started to work on the application layout.

--- pages/rensPage.py
+++ pages/rensPage.py
@@ -1,8 +1,68 @@
+from dash import Dash
 import dash_html_components as html
-
+import dash_ui as dui
+import dash_core_components as dcc
 
 def serve_layout():
-    return(html.Div(className="container", children=[
-        html.H1("Rens heeft een pagina"),
-        html.A(children="link", href="www.youtube.com", target="blank")
-    ]))
+    return(html.Div(
+        className="row",
+        children=[
+            html.Div(
+                className="six columns",
+                children=[
+                    html.Div(
+                        children=dcc.Graph(
+                            id='left-graph',
+                            figure={
+                                'data': [{
+                                    'x': [1, 2, 3],
+                                    'y': [3, 1, 2],
+                                    'type': 'bar'
+                                }],
+                                'layout': {
+                                    'height': 400,
+                                    'margin': {
+                                        'l': 10, 'b': 20, 't': 0, 'r': 0
+                                    }
+                                }
+                            }
+                        )
+                    )
+                ]
+            ),
+            html.Div(
+                className="six columns",
+                children=html.Div([
+                    dcc.Graph(
+                        id='right-top-graph',
+                        figure={
+                            'data': [{
+                                'x': [1, 2, 3],
+                                'y': [3, 1, 2],
+                                'type': 'bar'
+                            }],
+                            'layout': {
+                                'height': 200,
+                                'margin': {'l': 10, 'b': 20, 't': 0, 'r': 0}
+                            }
+                        }
+                    ),
+                    dcc.Graph(
+                        id='right-bottom-graph',
+                        figure={
+                            'data': [{
+                                'x': [1, 2, 3],
+                                'y': [3, 1, 2],
+                                'type': 'bar'
+                            }],
+                            'layout': {
+                                'height': 200,
+                                'margin': {'l': 10, 'b': 20, 't': 0, 'r': 0}
+                            }
+                        }
+                    ),
+
+                ])
+            )
+        ]))
+
