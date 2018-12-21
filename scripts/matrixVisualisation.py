6c4e263d0eaa2afdb002f42ba643258431930650
tomknip <45173142+tomknip@users.noreply.github.com>
2018-12-21 21:19:33 +0100

    Reordering Matrix Reordering function has been added to the class. Comments still need to be added.

--- scripts/matrixVisualisation.py
+++ scripts/matrixVisualisation.py
@@ -3,8 +3,10 @@ import plotly.graph_objs as go
 import plotly.plotly as py
 from plotly import offline
 from scripts import dataSelection as ds
+from scripts import preProcessing as pp
 import time as t
 import inspect
+import scipy.sparse as sc
 
 class AdjacencyMatrix:
     def __init__(self, filename):
@@ -16,13 +18,13 @@ class AdjacencyMatrix:
         self.x = []
         self.data = {}
         self.weight = []
-        matrix, self.mintime, self.maxtime = ds.aggravate(self.filename, max_time=max_time, min_time=min_time)
-        for i in range(matrix.shape[0]):
-            for j in range(matrix.shape[1]):
-                if matrix[i,j] != 0:
-                    self.data[i,j] = matrix[i,j]
+        self.matrix, self.mintime, self.maxtime = ds.aggravate(self.filename, max_time=max_time, min_time=min_time)
+        for i in range(self.matrix.shape[0]):
+            for j in range(self.matrix.shape[1]):
+                if self.matrix[i,j] != 0:
+                    self.data[i,j] = self.matrix[i,j]
                     self.x.append(i)
-                    self.weight.append(matrix[i,j])
+                    self.weight.append(self.matrix[i,j])
 
 
     def draw_plot(self, colorscale=None, xrange=None, weightrange=None, timerange=None):
@@ -80,3 +82,33 @@ class AdjacencyMatrix:
         if not self.data:
             self.get_data()
         return [self.mintime, self.maxtime]
+
+    def reorder(self):
+        sparse_matrix = sc.csr_matrix(self.matrix)
+        index_order = sc.csgraph.reverse_cuthill_mckee(sparse_matrix) # returns order of new matrix
+        index_mapping = dict()
+        for i in range(len(index_order)):
+            index_mapping[index_order[i]] = i
+
+
+        working_dir = pp.get_working_dir()
+        with open(working_dir + self.filename, 'r') as f:
+            encoded_data = f.read()
+        new_data = [i.strip().split(" ") for i in encoded_data.split('\n') if i != ""]
+        reorder_x = []
+        reorder_y = []
+        reorder_weight = []
+        for i in new_data[1:]:
+            if len(i) == 4:
+                reorder_x.append(index_mapping[int(i[1])])
+                reorder_y.append(index_mapping[int(i[2])])
+                reorder_weight.append(int(i[3]))
+        reorder_layout = go.Layout(hovermode='closest', width= 800,
+                            height = 800, margin={
+            'l': 25, 'b': 17, 't': 10, 'r': 5})
+        fig = go.Figure([{'x': reorder_x, 'y': reorder_y, 'text': reorder_weight, 'type': 'scatter', 'mode': 'markers',
+                          'marker': {'color': np.log(reorder_weight),
+                                     'colorbar': {'thickness': 3, 'title': "Log"}}
+                          }
+                         ], layout=reorder_layout)
+        return fig
