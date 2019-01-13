eef46c3d4f8019f159fbb8cc77fbdff28a59436f
Tom van meer <tom.vmeer@gmail.com>
2019-01-13 14:36:55 +0100

    Removed old dashboard stuff, polished UI

--- pages/dashboardPage.py
+++ pages/dashboardPage.py
@@ -67,7 +67,8 @@ class interactiveDashboard():
                                       style={'display': 'inline-table', 'width': '50%'}),
                             dcc.Input(id='shortestto', type='text', value=None,
                                       style={'display': 'inline-table', 'width': '50%'})]),
-                        html.Button('Execute changes', id='execute'),
+                        html.Div(className='textbox-small', children=html.Button('Execute changes', id='execute',
+                                                                                 className="button submit")),
                         # only here because callbacks need output:
                         html.Div(className='textbox-small', id='console_output')
                     ]
@@ -179,6 +180,21 @@ class interactiveDashboard():
             ]
         )
 
+    def bottom_right_container(self):
+        return html.Div(
+            className="two columns",
+            children=[
+                html.Div(
+                    id='bottom-right-container',
+                    className='window-small-bottom',
+                    style={
+                        'height': 300,
+                        'margin': {'l': 0, 'b': 0, 't': 0, 'r': 0}
+                    }
+                )
+            ]
+        )
+
 
 @app.callback(
     Output('console_output', 'children'),
@@ -280,172 +296,6 @@ def serve_layout():
             style={
                 'height': 300
             },
-            children=[dashboard.bottom_left_container(), dashboard.bottom_mid_menu()]
+            children=[dashboard.bottom_left_container(), dashboard.bottom_mid_menu(),
+                      dashboard.bottom_right_container()]
         )])
-
-#                 html.Div(
-#                     className="two columns",
-#                     children=[
-#                         html.Div(
-#                             id='bottom-right-container',
-#                             className='window-small-bottom',
-#                             style={
-#                                 'height':300,
-#                                 'margin': {'l': 0, 'b': 0, 't': 0, 'r': 0}
-#                             },
-#                             #children=[
-#                             #    html.Button('Run action', id='del-selection', className="button-option")
-#                             #]
-#                         )
-#                     ]
-#                   )
-#               ]
-#           )
-#     ])
-#
-#
-# #@app.callback(
-# #    Output('output-text', 'children'),
-# #    [Input('top-left-checkbox', 'values'), Input('del-selection', 'n_clicks')])
-# #def update_output(*value):
-# #    print("-----update_output----")
-# #    return ['You have selected "{}"'.format(value)]
-#
-# @app.callback(
-#     Output('node_slider', 'value'),
-#     [Input('matrix_plot', 'children')])
-# def update_silder(*args):
-#     print("-----update_slider----")
-#     return xrange_matrix_plot
-#
-# @app.callback(
-#     Output('matrix_plot', 'children'),
-#     [Input('top-left-dropdown', 'value'), Input('weight_slider', 'value'), Input('time_slider', 'value')])
-# def update_colorscale(colorscale, weight, timerange):
-#     print("-----update_colorscale----")
-#     global current_plot
-#     if current_plot:
-#         if type(timerange) != list:
-#             timerange = [timerange, timerange+1]
-#         return [mydcc.Relayout(id="relayout-adjacency", aim='adjacency_matrix'),
-#                 dcc.Graph(figure=matrix_plot.draw_plot(colorscale, weightrange=weight,
-#                                                        timerange=timerange), id='adjacency_matrix')]
-#     else:
-#         return None
-#
-#
-# @app.callback(
-#     Output('midplot', 'children'),
-#     [Input('time_slider', 'value'), Input('plot-selector', 'value'), Input('weight_slider', 'value'),
-#      Input('node_slider', 'value'), Input('shortestfrom', 'n_submit'), Input('shortestto', 'n_submit')],
-#     [State('shortestfrom', 'value'),
-#      State('shortestto', 'value')])
-# def update_midplot(timerange, to_plot, weightrange, noderange, n1, n2, dijkstafrom, dijkstrato):
-#     global timerangeG, to_ploG, weightrangeG, noderangeG, dijkstrafromG, dijkstratoG
-#     run = True
-#     if timerangeG != timerange:
-#         timerangeG = timerange
-#         run = True
-#     if to_ploG != to_plot:
-#         to_ploG = to_plot
-#         run = True
-#     if weightrangeG != weightrange:
-#         weightrangeG = weightrange
-#         run = True
-#     #if noderangeG != noderange:
-#     #    noderangeG = noderange
-#     #    run = True
-#     if dijkstrafromG != dijkstafrom:
-#         dijkstrafromG = dijkstafrom
-#         run = True
-#     if dijkstratoG != dijkstrato:
-#         dijkstratoG = dijkstrato
-#         run = True
-#     if noderange != noderangeG and to_plot == 'Interleaved dynamic network':
-#         noderangeG = noderange
-#         run = False
-#     if run:
-#         print("-----update_midplot----")
-#         global current_plot
-#         current_plot = to_plot
-#         print(to_plot)
-#         if to_plot == 'Radial' or to_plot == 'Fruchterman-Reingold':
-#             return dcc.Graph(id='mid-graph-t',
-#                              style={'margin':'auto'},
-#             figure=node_link_plot.draw_plot(timerange, to_plot, weightrange=weightrange, noderange=noderange,
-#                                             dijkstrafrom=dijkstafrom, dijkstrato=dijkstrato))
-#         elif to_plot == 'Interleaved dynamic network':
-#             return [mydcc.Relayout(id="relayout-midgraph", aim='mid-graph-t'), dcc.Graph(id='mid-graph-t',
-#                              style={'margin': 'auto'},
-#                              figure=
-#                              interleavedPlot.draw_interleaved("profile_semantic_trafo_final.txt",
-#                                                               start_time=timerange[0], end_time=timerange[1],
-#                                                               weight_start=weightrange[0], weight_end=weightrange[1]))]
-#
-# @app.callback(
-#     Output('bottom-middle-container', 'children'),
-#     [Input('plot-selector', 'value')]
-# )
-# def update_plot_tools(plot):
-#     print("-----update_plot_tools----")
-#     global current_plot
-#     current_plot = plot
-#     if plot == 'Radial' or plot == 'Fruchterman-Reingold':
-#         return [
-#             html.H6("Select node-range: ", className='mid-text'),
-#             rangeSlider.draw('node_slider', xrange_matrix_plot[0], xrange_matrix_plot[1],
-#                              int(xrange_matrix_plot[1] / 10)),
-#             html.H6("Select log weight-range: ", className='mid-text'),
-#             rangeSlider.draw('weight_slider', weightrange_matrix_plot[0], weightrange_matrix_plot[1],
-#                              int(weightrange_matrix_plot[1] / 10)),
-#             html.H6("Select time-range: ", className='mid-text'),
-#             slider.draw('time_slider', timerange_matrix_plot[0], timerange_matrix_plot[1],
-#                              int(timerange_matrix_plot[1] / 10)),
-#             html.Section(className='mid-text', id='output-text')
-#         ]
-#     elif plot == 'Interleaved dynamic network':
-#         return [
-#             html.H6("Select node-range: ", className='mid-text'),
-#             rangeSlider.draw('node_slider', xrange_matrix_plot[0], xrange_matrix_plot[1],
-#                              int(xrange_matrix_plot[1] / 10)),
-#             html.H6("Select log weight-range: ", className='mid-text'),
-#             rangeSlider.draw('weight_slider', weightrange_matrix_plot[0], weightrange_matrix_plot[1],
-#                              int(weightrange_matrix_plot[1] / 10)),
-#             html.H6("Select time-range: ", className='mid-text'),
-#             rangeSlider.draw('time_slider', timerange_matrix_plot[0], timerange_matrix_plot[1],
-#                              int(timerange_matrix_plot[1] / 10)),
-#             html.Section(className='mid-text', id='output-text')
-#         ]
-#     else:
-#         return [
-#                 html.H6("Select node-range: ", className='mid-text'),
-#                 rangeSlider.draw('node_slider', xrange_matrix_plot[0], xrange_matrix_plot[1],
-#                                  int(xrange_matrix_plot[1] / 10), visible=False),
-#                 html.H6("Select log weight-range: ", className='mid-text'),
-#                 rangeSlider.draw('weight_slider', weightrange_matrix_plot[0], weightrange_matrix_plot[1],
-#                                  int(weightrange_matrix_plot[1] / 10), visible=False),
-#                 html.H6("Select time-range: ", className='mid-text'),
-#                 rangeSlider.draw('time_slider', timerange_matrix_plot[0], timerange_matrix_plot[1],
-#                                  int(timerange_matrix_plot[1] / 10), visible=False),
-#                 html.Section(className='mid-text', id='output-text')
-#         ]
-#
-#
-#
-# #@app.callback(
-# #    Output('selected-data', 'children'),
-# #    [Input('mid-graph-t', 'selectedData')])
-# #def display_selected_data(selectedData):
-# #    try:
-# #       print(ast.literal_eval(json.dumps(selectedData, indent=2)))
-# #    except ValueError:
-# #        pass
-#
-# @app.callback(
-#     Output('relayout-midgraph', 'layout'),
-#     [Input('node_slider', 'value')])
-# def adjust_xrange(rangeSlider_range):
-#        return {'yaxis':dict(
-#         range=rangeSlider_range
-#     )}
-#
